cb93c19db2a969c046d1837a84d86ad0ff22dcc8
karun arora <arorakarun786@gmail.com>
2018-08-12 02:04:10 +0530

    second

--- js/admob.js
+++ js/admob.js
@@ -5,3 +5,8 @@
  document.addEventListener("deviceready", onDeviceReady, false);
 
 
+admob.requestInterstitialAd({
+  publisherId:          "ca-app-pub-8445604951136736/1726497870",
+  interstitialAdId:     "ca-app-pub-8445604951136736/7062585032",
+  autoShowInterstitial: true
+});
