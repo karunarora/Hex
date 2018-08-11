0902263223c039f3b8e121b560f6815c4976d6c7
karun arora <arorakarun786@gmail.com>
2018-08-12 01:34:10 +0530

    f

--- js/admob.js
+++ js/admob.js
@@ -1,27 +1,7 @@
-function adSetter(){
-alert(navigator.userAgent);
-var admobid = {};
-// select the right Ad Id according to platform
-if( /(android)/i.test(navigator.userAgent) ) { 
-    admobid = { // for Android
-        banner: 'ca-app-pub-8445604951136736/1726497870',
-        interstitial: 'ca-app-pub-8445604951136736/7062585032'
-    };
-} 
-
-if(AdMob) AdMob.createBanner( {
-    //isTesting:true, //Remove this Before publishing your app
-    adId:admobid.banner, 
-    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
-    autoShow:true} );
-
-}
   function onDeviceReady(){
-  alert("device ready");
-      adSetter();
+ admob.createBannerView({publisherId:'ca-app-pub-8445604951136736/1726497870'});
   }
 
-
-window.load=function(){
  document.addEventListener("deviceready", onDeviceReady, false);
-}
+
+
