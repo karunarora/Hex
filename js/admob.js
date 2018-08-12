64aa78236a936e3cd371839c3343f8935640cd74
karun arora <arorakarun786@gmail.com>
2018-08-12 14:26:12 +0530

    Update admob.js

--- js/admob.js
+++ js/admob.js
@@ -1,12 +1,12 @@
   function onDeviceReady(){
  admob.createBannerView({publisherId:'ca-app-pub-8445604951136736/1726497870'});
+    admob.requestInterstitialAd({
+  publisherId:          "ca-app-pub-8445604951136736/1726497870",
+  interstitialAdId:     "ca-app-pub-8445604951136736/7062585032",
+  autoShowInterstitial: true
+});
   }
 
  document.addEventListener("deviceready", onDeviceReady, false);
 
 
-admob.requestInterstitialAd({
-  publisherId:          "ca-app-pub-8445604951136736/1726497870",
-  interstitialAdId:     "ca-app-pub-8445604951136736/7062585032",
-  autoShowInterstitial: true
-});
